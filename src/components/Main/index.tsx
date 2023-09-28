ce74b47b9a836aa58741704e4fc79ef291e7b500
sauloviniciust <sauloviniciust@gmail.com>
2023-09-28 07:27:41 -0300

    adicionando produtos

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -7,24 +7,79 @@ const Main = () => {
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-zinc-200 mb-6">Produtos</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          <div className="bg-zinc-700 rounded-lg shadow-black shadow-2xl overflow-hidden border-2 border-zinc-600">
+           
+            <div className="bg-zinc-700 rounded-lg shadow-black hover:bg-zinc-800 border-zinc-600   
+             scale-75 md:scale-90 lg:scale-90 xl:scale-100 transition overflow-hidden 
+             ease-in-out delay-100 text-center z-10 transform hover:scale-105 border-2 shadow-2xl">
               <img
-                src="https://scontent.fplu13-1.fna.fbcdn.net/v/t39.30808-6/318443891_828463434783910_2384505542741429708_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=49d041&_nc_ohc=LbmEfI7KomEAX_6tr66&_nc_ht=scontent.fplu13-1.fna&oh=00_AfBsSnNamiDr64CnfG1R9m0CfBkF5GKUaR587X5Lbv-OTg&oe=6517F163"
-                alt="Product Image"
-                className="w-full"
-              />
-              <div className="p-4">
-                <h3 className="text-xl font-bold text-black mb-2">
-                Conjunto LIVE neon
-                </h3>
-                <p className="text-zinc-300 font-bold">Nova cor neon LIVE e nova bolsa CCM neon com rosa prática</p>
+                src="Slider colcci.jpg"
+                alt="Slider colcci"
+                className="w-full"/>
+                <div className="p-4">
+                 <h3 className="text-xl font-bold text-black mb-2">Slider colcci</h3>
+                  <p className="text-zinc-300 font-bold">
+                  Slider colcci em varias cores</p>
                 <div className="mt-4">
-                  <span className="text-black font-bold">$99.99</span>
+                  <span className="text-black font-bold">R$ 139,90</span>
+                  <a href="#" className="ml-4"><Button children="Compre" /></a>
+                </div>
+              </div>
+            </div>
+
+            <div className="bg-zinc-700 rounded-lg shadow-black hover:bg-zinc-800 border-zinc-600   
+             scale-75 md:scale-90 lg:scale-90 xl:scale-100 transition overflow-hidden 
+             ease-in-out delay-100 text-center z-10 transform hover:scale-105 border-2 shadow-2xl">
+              <img
+                src="Sandália colcci.jpg"
+                alt="Product Image"
+                className="w-full"/>
+                <div className="p-4">
+                 <h3 className="text-xl font-bold text-black mb-2">Sandália colcci</h3>
+                  <p className="text-zinc-300 font-bold">
+                   Sandalia Colcci estampada</p>
+                <div className="mt-4">
+                  <span className="text-black font-bold">R$ 99.90</span>
                   <a href="#" className="ml-4"><Button children="Compre" /></a>
                 </div>
               </div>
             </div>
                 
+            <div className="bg-zinc-700 rounded-lg shadow-black hover:bg-zinc-800 border-zinc-600   
+             scale-75 md:scale-90 lg:scale-90 xl:scale-100 transition overflow-hidden 
+             ease-in-out delay-100 text-center z-10 transform hover:scale-105 border-2 shadow-2xl">
+              <img
+                src="VESTIDO COLCCI FITNESS.jpg"
+                alt="Product Image"
+                className="w-full"/>
+                <div className="p-4">
+                 <h3 className="text-xl font-bold text-black mb-2">Vestido Colcci Fitness</h3>
+                  <p className="text-zinc-300 font-bold">
+                   Vestidos colcci em varias cores e tecido super leve</p>
+                <div className="mt-4">
+                  <span className="text-black font-bold">R$ 169.90</span>
+                  <a href="#" className="ml-4"><Button children="Compre" /></a>
+                </div>
+              </div>
+            </div>
+
+            <div className="bg-zinc-700 rounded-lg shadow-black hover:bg-zinc-800 border-zinc-600   
+             scale-75 md:scale-90 lg:scale-90 xl:scale-100 transition overflow-hidden 
+             ease-in-out delay-100 text-center z-10 transform hover:scale-105 border-2 shadow-2xl">
+              <img
+                src="bolsa colcci.jpg"
+                alt="bolsa colcci"
+                className=" w-full h-96"/>
+                <div className="p-4">
+                 <h3 className="text-xl font-bold text-black mb-2">Bolsa colcci</h3>
+                  <p className="text-zinc-300 font-bold">
+                   Bolsa colcci em varias cores</p>
+                <div className="mt-4">
+                  <span className="text-black font-bold">R$ 289.90</span>
+                  <a href="#" className="ml-4"><Button children="Compre" /></a>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </section>
