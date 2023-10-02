56a165f459334fb337b54a9a246f2e13b6487f66
Sauloviniciust <sauloviniciust@gmail.com>
2023-10-02 19:35:26 -0300

    editando

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -3,13 +3,13 @@ import { Card } from "../Card";
 
 
 const Main = () => {
-  const imagem1 = (<img src="Slider colcci.jpg"alt="Slider colcci em varias cores, super confortavel"className="w-full"/>);
-  const imagem2 = (<img src="Sandália colcci.jpg"alt="Sandalia colcci em varias cores, super confortavel"className="w-full"/>);
-  const imagem3 = (<img src="VESTIDO COLCCI FITNESS.jpg"alt="Vestidos colcci em varias cores e tecido super leve"className="w-full"/>);
-  const imagem4 = (<img src="bolsa colcci.jpg"alt="Bolsa colcci em varias cores, para varias ocasioes"className="w-full"/>);
+  const imagem1 = (<img src="Slider colcci.jpg"alt="Slider colcci em varias cores, super confortavel"/>);
+  const imagem2 = (<img src="Sandália colcci.jpg"alt="Sandalia colcci em varias cores, super confortavel"/>);
+  const imagem3 = (<img src="VESTIDO COLCCI FITNESS.jpg"alt="Vestidos colcci em varias cores e tecido super leve"/>);
+  const imagem4 = (<img src="bolsa colcci.jpg"alt="Bolsa colcci em varias cores, para varias ocasioes"/>);
 
   return (
-    <>
+  
        <section className="bg-zinc-800 py-12">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-zinc-200 mb-6">Produtos</h2>
@@ -23,7 +23,7 @@ const Main = () => {
           </div>
         </div>
       </section> 
-    </>
+  
   );
 };
 
