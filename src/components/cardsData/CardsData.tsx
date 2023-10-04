92f0ddabefae8e9e9b2efe33b0d2bdb78ffede14
Sauloviniciust <sauloviniciust@gmail.com>
2023-10-04 14:37:24 -0300

    corrigindo erros

--- src/components/cardsData/CardsData.tsx
+++ src/components/cardsData/CardsData.tsx
@@ -1,40 +1,28 @@
 const cardsData = [
-    {
-      image: <img
-      src="Slider colcci.jpg"
-      alt="Slider colcci em varias cores, super confortavel"
-    />,
-      product: "Slider colcci",
-      about: "Slider colcci em varias cores, super confortavel",
-      price: "R$ 139,90",
-    },
-    {
-      image: <img
-      src="Sandália colcci.jpg"
-      alt="Sandalia colcci em varias cores, super confortavel"
-    />,
-      product: "Sandália colcci",
-      about: "Sandalia colcci em varias cores, super confortavel",
-      price: "R$ 99.90",
-    },
-    {
-      image: <img
-      src="VESTIDO COLCCI FITNESS.jpg"
-      alt="Vestidos colcci em varias cores e tecido super leve"
-    />,
-      product: "Vestido Colcci Fitness",
-      about: "Vestidos colcci em varias cores e tecido super leve",
-      price: "R$ 169.90",
-    },
-    {
-      image: <img
-      src="bolsa colcci.png"
-      alt="Bolsa colcci em varias cores, para varias ocasioes"
-    />,
-      product: "Bolsa colcci",
-      about: "Bolsa colcci em varias cores, para varias ocasioes",
-      price: "R$ 289.90",
-    },
-  ];
+  {
+    image: "Slider colcci.jpg",
+    product: "Slider colcci",
+    about: "Slider colcci em varias cores, super confortavel",
+    price: "R$ 139,90",
+  },
+  {
+    image: "Sandália colcci.jpg",
+    product: "Sandália colcci",
+    about: "Sandalia colcci em varias cores, super confortavel",
+    price: "R$ 99.90",
+  },
+  {
+    image: "VESTIDO COLCCI FITNESS.jpg",
+    product: "Vestido Colcci Fitness",
+    about: "Vestidos colcci em varias cores e tecido super leve",
+    price: "R$ 169.90",
+  },
+  {
+    image: "bolsa colcci.png",
+    product: "Bolsa colcci",
+    about: "Bolsa colcci em varias cores, para varias ocasioes",
+    price: "R$ 289.90",
+  },
+];
 
-  export {cardsData}
+export { cardsData };
