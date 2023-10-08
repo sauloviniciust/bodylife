590eae2e263cdbded9eed006188dd7001dacc87f
Sauloviniciust <sauloviniciust@gmail.com>
2023-10-08 00:36:31 -0300

    editando home

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Home } from './Pages/Home/index.tsx'
+
 
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import { ItemPage } from './Pages/Item'
+import { Home } from './Pages/Home/index.tsx'
 
 
 const router = createBrowserRouter([
